1c9776bc765c10e5aaa7d7247576d18a2637855f
imdevedugame <saranghae1133@gmail.com>
2025-10-13 22:00:26 +0700

    menggunakan variant class tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const courses = [
+    {
+      title: "System Administration and IT Infrastructure Services",
+    },
+    {
+      title: "Operating Systems Becoming a Power User",
+    },
+    {
+      title: "The Bits and Bytes of Computer Networking",
+    },
+    {
+      title: "Technical Support Fundamentals",
+    },
+    {
+      title: "How to Succeed at: Writing Applications",
+    },
+    {
+      title: "Medicine Administration for Carers",
+    },
+  ];
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="p-6 grid grid-cols-1 gap-6">
+        {courses.map((course, index) => (
+          <div key={index} className="shadow rounded-lg overflow-hidden">
+            {/* Image */}
+            <img
+              src="https://www.placehold.co/200x400"
+              alt={course.title}
+              className="w-full h-80 object-cover"
+            />
+
+             <div className="p-4 bg-red-100">
+              <h3 className="text-lg font-semibold mb-4">{course.title}</h3>
+             							<div className="bg-red-50 p-2 rounded-lg">
+                <div className="text-sm text-gray-500 mt-2">
+                  <span>👥 123 users</span>
+                  <span>⏱ 60</span>
+                </div>
+
+                {/* Author */}
+                <div className="mt-3 gap-2">
+                  <img
+                    src="https://www.placehold.co/50x50"
+                    alt="Author's Avatar"
+                    className="w-12 h-12 rounded-full object-cover"
+                  />
+                  <div>
+                    <p className="text-sm font-medium">Author's Name</p>
+                    <p className="text-xs text-gray-500">Designer</p>
+                  </div>
+                </div>
+              </div>
+              
+                            {/* Footer */}
+              <div className="mt-4">
+                <div className="bg-white py-2 px-4 rounded">$123</div>
+                <button className="bg-red-700 text-white px-4 py-10 rounded text-sm">
+                  Get Started
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
-     <h1 className="text-3xl font-bold underline">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
