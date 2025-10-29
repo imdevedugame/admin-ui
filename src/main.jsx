f15317fd02b2ad4104958349f74d61c2b680a395
imdevedugame <saranghae1133@gmail.com>
2025-10-30 01:42:48 +0700

    updatye tugas 6

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import "./App.css"
-import Exercise from "../components/Exercise";
+import Exercise from "../components/Exercise.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
